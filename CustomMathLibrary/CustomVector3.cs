6119bb1253b7de2109215e9ce15b37a23a13d29b
Shorayn <34161708+Shorayn@users.noreply.github.com>
2020-02-25 15:10:31 +0100

    - expanded CustomVector3.cs - added new functions to LinearAlgebra.cs to solve the DetermineDirectionVectorForSymmetryLine function

--- CustomMathLibrary/CustomVector3.cs
+++ CustomMathLibrary/CustomVector3.cs
@@ -4,8 +4,6 @@ namespace CustomMathLibrary
 {
     public struct CustomVector3
     {
-        
-
         public float x;
         public float y;
         public float z;
@@ -16,6 +14,8 @@ namespace CustomMathLibrary
             this.y = y;
             this.z = z;
         }
+        
+        private static readonly CustomVector3 zeroCusVec = new CustomVector3(0.0f,0.0f,0.0f);
 
         public static CustomVector3 operator +(CustomVector3 a, CustomVector3 b)
         {
@@ -76,15 +76,44 @@ namespace CustomMathLibrary
             }
         }
 
-        // TODO: CONTINUE HERE
-        public static CustomVector3 Normalize(CustomVector3 a)
+        public void Normalize()
         {
-            return new CustomVector3();
+            float num = CustomVector3.Magnitude(this);
+            if ((double) num > 9.99999974737875E-06)
+                this = this / num;
+            else
+                this = CustomVector3.zero;
         }
 
+        public static CustomVector3 Normalize(CustomVector3 vector)
+        {
+            float number = CustomVector3.Magnitude(vector);
+            return (double) number > 9.99999974737875E-06 ? vector / number : CustomVector3.zero;
+        }
+
+        public CustomVector3 normalized => CustomVector3.Normalize(this);
+
         /// <summary>
-        /// Return the length of the vector
+        /// Returns the length of the vector
         /// </summary>
         public float magnitude => (float) Math.Sqrt((double) this.x * (double) this.x + (double) this.y * (double) this.y + (double) this.z * (double) this.z);
+
+        public static float Magnitude(CustomVector3 a)
+        {
+            return (float) Math.Sqrt((double) a.x * (double) a.x + (double) a.y * (double) a.y + (double) a.z * (double) a.z);
+        }
+
+        public static CustomVector3 zero => CustomVector3.zeroCusVec;
+
+
+        public static CustomVector3 Cross(CustomVector3 lhs, CustomVector3 rhs)
+        {
+            return new CustomVector3((float) ((double) lhs.y * (double) rhs.z - (double) lhs.z * (double) rhs.y), (float) ((double) lhs.z * (double) rhs.x - (double) lhs.x * (double) rhs.z), (float) ((double) lhs.x * (double) rhs.y - (double) lhs.y * (double) rhs.x));
+        }
+
+        public static float Dot(CustomVector3 lhs, CustomVector3 rhs)
+        {
+            return (float) ((double) lhs.x * (double) rhs.x + (double) lhs.y * (double) rhs.y +(double) lhs.z * (double) rhs.z);
+        }
     }
 }
--- CustomMathLibrary/LinearAlgebra.cs
+++ CustomMathLibrary/LinearAlgebra.cs
@@ -15,9 +15,9 @@ namespace CustomMathLibrary
         /// <returns>The coefficients d, a, b, c of the new plane equation in this order</returns>
         public static float[] CopyPlaneAndMoveAlongNormal(float[] normal, float[] pointOnOriginalPlane, int factor)
         {
-           
             CustomVector3 convertedNormal = CustomVector3Converter.FloatArrayToCustomVector3(normal);
-            CustomVector3 convertedPointOnOriginalPlane = CustomVector3Converter.FloatArrayToCustomVector3(pointOnOriginalPlane);
+            CustomVector3 convertedPointOnOriginalPlane =
+                CustomVector3Converter.FloatArrayToCustomVector3(pointOnOriginalPlane);
 
             CustomVector3 pointOnCopiedPlane = convertedPointOnOriginalPlane + convertedNormal * factor;
             float dNew = -(convertedNormal.x * pointOnCopiedPlane.x + convertedNormal.y * pointOnCopiedPlane.y +
@@ -25,9 +25,17 @@ namespace CustomMathLibrary
 
             return new[] {dNew, convertedNormal.x, convertedNormal.y, convertedNormal.z};
         }
-        
-        
-        private float[] DetermineDirectionVectorForSymmetryLine(float[] normal, float[] direction)
+
+
+        /// <summary>
+        ///
+        /// Determines the direction vector for a symmetry line
+        ///  
+        /// </summary>
+        /// <param name="normal"></param>
+        /// <param name="direction"></param>
+        /// <returns>The direction vector</returns>
+        public static float[] DetermineDirectionVectorForSymmetryLine(float[] normal, float[] direction)
         {
             /*
                  *	Fuer Symmetrieebene/gerade muss folgendes gelten:
@@ -45,14 +53,20 @@ namespace CustomMathLibrary
                  * 
                  */
 
-            Matrix<float> A = Matrix<float>.Build.DenseOfArray(new float[,]
+            Matrix<float> a = Matrix<float>.Build.DenseOfArray(new float[,]
             {
                 {direction[0], direction[1], direction[2]},
                 {normal[0], normal[1], normal[2]},
                 {0, 0, 0}
             });
+
+            return SolveUnderdeterminedLinearEquation(a);
+
+            /*
             
-            QR<float> qr = A.QR();
+                        OLD VERSION
+            
+            QR<float> qr = a.QR();
             Matrix<float> q = qr.Q;
             Matrix<float> r = qr.R;
 
@@ -72,26 +86,88 @@ namespace CustomMathLibrary
                 vPortion[i] /= r[i, i];
                 wPortion[i] /= r[i, i];
             }
-
-
-            //Debug.Log(wPortion);
-
+            
             Vector<float> rV = q.Transpose() * vPortion;
             Vector<float> rW = q.Transpose() * wPortion;
 
 
-            //  r = (r1,r2,r3) wobei bspw. xi = vi + lambda * wi ist
+            //  r = (r1,r2,r3) wobei bspw. ri = vi + lambda * wi ist
             float r1 = rV[0] + 1 * rW[0];
             float r2 = rV[1] + 1 * rW[1];
             float r3 = rV[2] + 1 * rW[2];
 
-            CustomVector3 result = new CustomVector3(r1, r2, r3);
+            CustomVector3 tempResult = new CustomVector3(r1, r2, r3);
 
-            return null;
-            //return result.normalized;
+            float[] result = CustomVector3Converter.CustomVector3ToFloatArray(tempResult.normalized);
+
+            return result;
+            */
         }
-        
-        
-       
+
+        /// <summary>
+        ///  Solves an underdetermined linear equation system with 2 equations and 3 unknown.
+        /// </summary>
+        /// <param name="matrix">The 3x3 matrix, which represents the linear equation system</param>
+        /// <returns>One possible solution, where the parameter is set as 1.</returns>
+        public static float[] SolveUnderdeterminedLinearEquation(Matrix<float> matrix)
+        {
+            Matrix<float>[] qr = QRDecomposition(matrix);
+            Vector<float>[] portions = GaussianEliminationUnderdeterminedEq(qr[1]);
+            Vector<float> rV = qr[0].Transpose() * portions[0];
+            Vector<float> rW = qr[0].Transpose() * portions[1];
+
+
+            //  r = (r1,r2,r3) wobei bspw. ri = vi + lambda * wi ist
+            float r1 = rV[0] + 1 * rW[0];
+            float r2 = rV[1] + 1 * rW[1];
+            float r3 = rV[2] + 1 * rW[2];
+
+            CustomVector3 tempResult = new CustomVector3(r1, r2, r3);
+            float[] result = CustomVector3Converter.CustomVector3ToFloatArray(tempResult.normalized);
+
+            return result;
+        }
+
+        /// <summary>
+        /// Solves a given 3x3 triangular matrix using the gaussian elimination.
+        /// The matrix has to represent an underdetermined linear equation system. The last row has to be filled with zeros.
+        /// </summary>
+        /// <param name="rMatrix">The matrix to solve</param>
+        /// <returns>The two parts of the solution</returns>
+        public static Vector<float>[] GaussianEliminationUnderdeterminedEq(Matrix<float> rMatrix)
+        {
+            Vector<float> vPortion = Vector<float>.Build.DenseOfArray(new[] {0f, 0, 0});
+            Vector<float> wPortion = Vector<float>.Build.DenseOfArray(new[] {0f, 0, 1});
+
+            for (int i = rMatrix.RowCount - 2; i >= 0; i--)
+            {
+                // Von rechts ohne das Diagonalelement j > i
+                for (int j = rMatrix.ColumnCount - 1; j > i; j--)
+                {
+                    vPortion[i] -= rMatrix[i, j] * vPortion[j];
+                    wPortion[i] -= rMatrix[i, j] * wPortion[j];
+                }
+
+                vPortion[i] /= rMatrix[i, i];
+                wPortion[i] /= rMatrix[i, i];
+            }
+
+            return new[] {vPortion, wPortion};
+        }
+
+
+        /// <summary>
+        /// Computes the QR-Decomposition of a given m x n matrix (uses the MathNet.Numerics library).
+        /// </summary>
+        /// <param name="matrix">The input matrix</param>
+        /// <returns>The Q and R matrix</returns>
+        public static Matrix<float>[] QRDecomposition(Matrix<float> matrix)
+        {
+            QR<float> qr = matrix.QR();
+            Matrix<float> q = qr.Q;
+            Matrix<float> r = qr.R;
+
+            return new[] {q, r};
+        }
     }
 }
